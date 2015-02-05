2f3d46d27727ae900557d60a5ebcdefabde3e393
Jonathan Quist <jonathan.quist@banno.com>
2015-02-04 18:00:54 -0600

    added in support for cd, exiting

---
+++ README.txt
@@ -0,0 +1,6 @@
+Names: Jonathan Quist, Cole Boudreau
+Files: shell.c - The main file, contains the main method and a method for working with the array of tokens
+Instructions: To compile simply run 'gcc shell.c -o shell'
+              To run type './shell' and you are ready to go
+Challenges: Figuring out how to store an array of strings
+Sources: http://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
--- shell.c
+++ shell.c
@@ -7,8 +7,11 @@
 #include <strings.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #define BUFFER_LENGTH 80
-void run_commands(char * commands[], size_t n);
+int run_commands(char * commands[], size_t n);
+int run_built_in_commands(char * commands[], size_t n);
 int main() {
   int cont = 1;
   char input[BUFFER_LENGTH]; //words inputted by user
@@ -21,36 +24,56 @@ int main() {
   while(cont == 1) {
     printf("prompt$ ");
     scanf("%[^\n]%*c",input);
-    if (strncmp(input,"exit", strlen("exit")) == 0){
-      cont = 0;
-    }
-    else {
-      token = strtok(input, s);
 
-      while(token != NULL) {
-        tokens[n++] = token;
-        token = strtok(NULL, s);
-      }
-      run_commands(tokens, n);
+    token = strtok(input, s);
+
+    while(token != NULL) {
+      tokens[n++] = token;
+      token = strtok(NULL, s);
     }
+    tokens[n++] = token;
+    cont = run_commands(tokens, n); //run_commands returns an int, if 0 is returned that means the user would like to exit
   }
   printf("Exiting!\n");
 }
 
 //takes in a array of tokens and the array's size. Parses through and runs the commands
-void run_commands(char * commands[], size_t n) {
+int run_commands(char * commands[], size_t n) {
   pid_t pid;
-  size_t i;
-  for (i = 0; i != n; ++i)
-  {
-    printf("%s\n", commands[i]);
+  int status;
+  int cont = 1;
+  if (strncmp(commands[0],"exit", strlen("exit")) == 0 || strncmp(commands[0],"cd", strlen("cd")) == 0) {
+    cont = run_built_in_commands(commands, n);
   }
-  //pid = fork();
-//        if (pid >= 0) {
-//          printf("\n%s\n", token);
-//        token = strtok(NULL, s);
+  else {
+    if ((pid = fork()) == 0) {
+      int command_status;
+      command_status = execvp(commands[0], commands);
+      exit(command_status);
+    }
+    else {
+      waitpid(-1, &status, 0);
+      if (status != 0) {
+        printf("Error: Command could not be executed\n");
+      }
+    }
+  }
+  return cont;
+}
 
-//        } else {
-//          printf("Error: command not be executed.");
-//        }
+int run_built_in_commands(char * commands[], size_t n) {
+  if (strncmp(commands[0],"exit", strlen("exit")) == 0) {
+    return 0;
+  } else {
+    int j;
+    char * pwd = getenv("PWD");
+    char * parent_location = strrchr(pwd, '/');
+    *parent_location = '\0';
+    j = chdir(pwd);
+    printf("%d\n\n", j);
+    setenv("PWD", pwd , 1);
+    printf("%s\n",getenv("PWD"));
+
+    return 1;
+  }
 }
